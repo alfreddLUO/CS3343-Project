1db4bac341c321ebf5a95e4893e8568525b7c0e0
S0b3Rr <94901095+S0b3Rr@users.noreply.github.com>
2022-09-17 14:34:29 +0800

    Delete CS3343_Project directory

--- CS3343_Project/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author Administrator
- *
- */
-module CS3343_Project {
-}
--- CS3343_Project/src/testGit/TestGit.java
+++
@@ -1,7 +0,0 @@
-package testGit;
-
-public class TestGit {
-	public static void main(String[] args) {
-		System.out.println("hello");
-	}
-}
