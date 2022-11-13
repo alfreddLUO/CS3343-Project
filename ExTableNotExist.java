31deaf101c99828126a7d9ff4d4b10f3d7f3fa42
S0b3Rr <94901095+S0b3Rr@users.noreply.github.com>
2022-11-13 11:33:23 +0800

    Update ExTableNotExist.java

--- ExTableNotExist.java
+++ ExTableNotExist.java
@@ -2,7 +2,7 @@
 public class ExTableNotExist extends Exception {
     public ExTableNotExist(int tableId) {
         super(String.format(
-                "Can't delete the table with id of %d. Maybe due to it's unavailability or it doesn't exist.\n",
+                "Table with ID: %d does not exist!\n",
                 tableId));
     }
 }
